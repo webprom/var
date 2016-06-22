9426645aa94320d88bc2f008b689f3793961ec3d
Iggy Pritzker <iggyprom@gmail.com>
2016-06-22 04:10:10 +0300

    Making this work with H2 headings & removing links
    No need for links on headings. Leaving anchors near headings.

--- Zendesk/inject table of contents.js
+++ Zendesk/inject table of contents.js
@@ -4,26 +4,22 @@
  * @license MIT License
  * @author Rutger Meekers [rutger@meekers.eu]
  */
-if ($('.article-body span.wysiwyg-font-size-x-large').length > 2) {
+if ($('.article-body h2').length > 2) {
 
   // Create an array to store the TOC
   var toc = [];
 
   // Find all the headers, create a list from them and add name attributes
-  $('.article-body span.wysiwyg-font-size-x-large').replaceWith(function() {
+  $('.article-body h2').replaceWith(function() {
     // Get the heading text
     var headingText = $.trim($(this).text());
     // Add the text to the menu
     toc.push(headingText);
-    return '<a name="' +
-            headingText +
-            '" class="wysiwyg-font-size-x-large article-heading-link">' +
-            headingText +
-            '</a>';
+    return '<a name="' + headingText + '"></a><h2>' +  headingText + '</h2>';
   });
 
   // Create the TOC placeholder
-  $('.article-body').prepend('<div class="article-table-of-contents"><span class="wysiwyg-font-size-large">In This Article:</span><ol></ol></div>');
+  $('.article-body').prepend('<div class="article-table-of-contents"><ol></ol></div>');
 
   // Add the TOC to the placeholder
   $(toc).each(function(index) {
